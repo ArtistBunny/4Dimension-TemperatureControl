898b44c4f5657f7488e85f4f899ea30284d00409
twelvequalbunny <twelvequalbunny@users.noreply.github.com>
2021-09-24 12:40:12 +0800

    buildV2.1

---
+++ OBJ/core_cm3.d
@@ -0,0 +1,2 @@
+.\obj\core_cm3.o: LIB\src\core_cm3.c
+.\obj\core_cm3.o: D:\Program Files\Keli_V5Core\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ OBJ/startup_stm32f10x_hd.d
@@ -0,0 +1,1 @@
+.\obj\startup_stm32f10x_hd.o: LIB\Startup\startup_stm32f10x_hd.s
